6c032db54ef3707ae517874a4042ab5d00883c22
shailajasivakumar <48519280+shailajasivakumar@users.noreply.github.com>
2019-03-13 19:16:24 +0530

    Create mulities.c

---
+++ mulities.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n,i=1;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    for(i=1;i<n;++i)
+    {
+      printf("\n 5 * %ld = %ld",i,5*i);
+    }
+   
+    
+    
+}
+    
