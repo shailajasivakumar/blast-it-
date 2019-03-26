553f39884d1aa1f1cd33e030b55830150bbdad24
shailajasivakumar <48519280+shailajasivakumar@users.noreply.github.com>
2019-03-26 10:35:33 +0530

    Create 3c.c

---
+++ 3c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  int n,v,c,i,sum=0;
+  printf("enter the value",n,v,c);
+  scanf("%d" "%d" "%d",&n,&v, &c);
+  for(i=n;i>1;i--)
+  {
+  sum=sum+(v+(n-1)*c);
+  }
+}
